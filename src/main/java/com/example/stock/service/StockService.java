682b9d32e2674bbc821ec146c49ad2d80d11ce81
이준혁 <merong245@gmail.com>
2024-02-08 19:49:00 +0900

    [Feat] : Syncronized 키워드로 동시성 해결

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -13,7 +14,7 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    public void decrease(Long id, Long quantity){
+    public synchronized void decrease(Long id, Long quantity){
         // 재고 조회
         // 재고 감소
         // 갱신된 값 저장
