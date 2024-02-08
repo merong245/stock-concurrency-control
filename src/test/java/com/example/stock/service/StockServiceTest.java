cf86b217e2b06e2e44f6360bf5fc7e419e249103
이준혁 <merong245@gmail.com>
2024-02-08 19:44:17 +0900

    [Test] : 동시성 문제가 발생하는 테스트 생성

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -39,5 +44,29 @@ class StockServiceTest {
 
     }
 
+    @Test
+    @DisplayName("재고감소_동시100개")
+    public void decrease_stock_multi() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount); // 100개 끝날때까지 기다려야함
 
+        for(int i= 0; i < threadCount; i++){
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                }
+                finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - 1(100) = 0; -> 실패
+        assertEquals(0, stock.getQuantity());
+
+    }
 }
